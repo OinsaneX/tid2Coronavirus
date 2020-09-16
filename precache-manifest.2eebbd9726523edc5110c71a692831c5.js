72ed858cb0d38308e2c9dc98d61ec5af9c86b323
OinsaneX <alejandroalfonsovargas@gmail.com>
2020-09-16 15:40:26 -0700

    Updates

---
+++ precache-manifest.2eebbd9726523edc5110c71a692831c5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593236966f6c8df61b5d2d1e88af4c97",
+    "url": "/tid2Coronavirus/index.html"
+  },
+  {
+    "revision": "c69d4d1bb05c1343b3c0",
+    "url": "/tid2Coronavirus/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fd40faa29d40519fa31a",
+    "url": "/tid2Coronavirus/static/css/main.00e415fa.chunk.css"
+  },
+  {
+    "revision": "c69d4d1bb05c1343b3c0",
+    "url": "/tid2Coronavirus/static/js/2.9057a520.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/tid2Coronavirus/static/js/2.9057a520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd40faa29d40519fa31a",
+    "url": "/tid2Coronavirus/static/js/main.e882f31c.chunk.js"
+  },
+  {
+    "revision": "758f4c233e57d47b15ab",
+    "url": "/tid2Coronavirus/static/js/runtime-main.89b36858.js"
+  },
+  {
+    "revision": "e6ec6ac8da59088f70b99f7b764fb729",
+    "url": "/tid2Coronavirus/static/media/1.e6ec6ac8.jpg"
+  },
+  {
+    "revision": "fa42c5f5a6fc2a5b2f1100475d540585",
+    "url": "/tid2Coronavirus/static/media/2.fa42c5f5.jpg"
+  },
+  {
+    "revision": "57c1ec7658e69ff19af90af93dd61f96",
+    "url": "/tid2Coronavirus/static/media/3.57c1ec76.jpg"
+  },
+  {
+    "revision": "1703d75ad48a115919babb8649f01075",
+    "url": "/tid2Coronavirus/static/media/login.1703d75a.jpeg"
+  }
+]);
